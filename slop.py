6f390fa1e6618c6ad2693c2c8973b01bde25ba28
pankaj gupta <pg925303@gmail.com>
2022-11-28 14:23:48 +0530

    miner fixes

--- slop.py
+++ slop.py
@@ -1,7 +1,7 @@
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.screenmanager import MDScreenManager
 from kivy.core.text import LabelBase
 from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
@@ -11,7 +11,7 @@ Window.size = (310, 500)
 
 class SlopeApp(MDApp):
     def build(self):
-        screen_manager = ScreenManager()
+        screen_manager = MDScreenManager()
         screen_manager.add_widget(Builder.load_file("main.kv"))
         screen_manager.add_widget(Builder.load_file("login.kv"))
         screen_manager.add_widget(Builder.load_file("signup.kv"))
@@ -19,7 +19,7 @@ class SlopeApp(MDApp):
 
 
 if __name__ == "__main__":
-    LabelBase.register(name="Lato", fn_regular="Lato/Lato-Regular.ttf")
-    LabelBase.register(name="Lato", fn_regular="Lato/Lato-Bold.ttf")
+    LabelBase.register(name="Lato", fn_regular="Lato\\Lato-Regular.ttf")
+    LabelBase.register(name="Lato", fn_regular="Lato\\Lato-Bold.ttf")
 
     SlopeApp().run()

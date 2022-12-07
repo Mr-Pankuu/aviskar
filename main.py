c0968bc018f3ad7943ad6fb1d38c4c4b905f0352
nico-zero <zandaxheart@gmail.com>
2022-12-07 17:06:06 +0530

    updating...

--- main.py
+++ main.py
@@ -11,16 +11,27 @@ CLIENT = MongoClient("mongodb://localhost:27017")
 Config.set("kivy", "keyboard_mode", "systemanddock")
 
 Window.size = (310, 500)
+last_screen = []
 
 
 class Main(MDScreen):
-    def email(self, data):
-        print(type(data))
-
+    def on_enter(self):
+        global last_screen
+        last_screen = []
+        last_screen.append("main")
+        print("this" + self.name + "this")
 
 class Login(MDScreen):
-    def email(self, data):
-        print(data)
+    def back(self):
+        global last_screen
+        last_screen.pop()
+        print(last_screen)
+        self.manager.current = last_screen[-1]
+
+    def enter(self):
+        last_screen.append(self.name)
+        print("jj")
+        print(last_screen)
 
     def login(self):
         email = self.ids.email_data.text
@@ -35,6 +46,18 @@ class Login(MDScreen):
 
 
 class Signup(MDScreen):
+    def back(self):
+        global last_screen
+        last_screen.pop()
+        print(last_screen)
+        self.manager.current = last_screen[-1]
+
+    def enter(self):
+        last_screen.append(self.name)
+        print("jj")
+        print(last_screen)
+
+
     def sign_up(self):
         data = {
             "username": self.ids.user_name_data.text,
